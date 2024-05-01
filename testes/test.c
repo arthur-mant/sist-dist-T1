bfa151d711de589b187cd76f565fc6d2c8af9fbc
Arthur Martinelli <ama18@inf.ufpr.br>
2024-05-01 19:20:40 -0300

    c(i, s) agr retorna conjuntos na ordem correta

--- testes/test.c
+++ testes/test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int power(int b, int e) {
 
@@ -9,20 +10,19 @@ int power(int b, int e) {
     return result;
 }
 
-int c(int i, int s) {
-
-    if (s == 0)
-        return i;
+void c(int i, int s, int *v) {
 
     int aux = power(2, s);
     int highest_potency = 0;
+    //determinando o tamanho mínimo do cluster que contém esse id
     while (i > power(2, highest_potency))
         highest_potency++;
 
+    int pivo;
     int reduced_id = i;
     int potencias = power(2, highest_potency);
 
-    //reduzindo
+    //reduzindo o i para o equivalente da posição no cluster de menor id possível
     while (aux <= reduced_id) {
         if (reduced_id >= potencias) {
             reduced_id -= potencias;
@@ -30,25 +30,51 @@ int c(int i, int s) {
         potencias = potencias/2;
     }
 
+    //tendo sua posição na árvore simplificada, usa o simétrico em relação cluster s para encontrar o primeiro testador
     if (reduced_id < aux/2)
-        return i+aux/2;
+        pivo = i+aux/2;
     else
-        return i-aux/2;
+        pivo = i-aux/2;
+
+    //aqui, damos o nome de pivô para o primeiro candidato a testador
+    //inicia-se nele, e percorre todos os id dentro do cluster s dele
+
+    int j = pivo % (aux/2);
+    int base = pivo - j;
+    int count = 0;
+
+    while (count < aux/2) {
+
+        v[count] = base+j;
+
+        j = (j+1) % (aux/2);
+        count++;
+    }
+    v[count] = -2;      //indica fim da fila
 
 }
 
 int main() {
 
     int p=3;
+    int *v = (int *)malloc(sizeof(int)*power(2, p));
+
     for (int i=0; i<power(2, p); i++) {
 
         printf("%d: (", i);
-        for (int j=0; j<p; j++)
-            printf("%d, ", c(i, j+1));
+        for (int j=1; j<=p; j++) {
+            printf("[");
+            c(i, j, v);
+            int count = 0;
+            while (v[count] != -2) {
+                printf("%d, ", v[count]);
+                count++;
+            }
+
+            printf("]");
+        }
         printf(")\n");
 
     }
-
-    printf("11, 2: %d\n", c(11, 2));
 
 }
